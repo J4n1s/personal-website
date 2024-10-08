9649d7fe3d780bfc56baf6e6c9d3eeb711c8cc72
Janis Schneeberger <janis.schneeberger@outlook.com>
2024-10-08 18:36:43 +0200

    FEAT: Contact Form Validation and Styling

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -1,28 +1,38 @@
 import { Component } from '@angular/core';
-import {Validators, ReactiveFormsModule, FormBuilder} from "@angular/forms";
+import {Validators, ReactiveFormsModule, FormBuilder, FormGroup, FormControl} from "@angular/forms";
 import {ContactInformation} from "../../dtos/contact-information";
 import {ContactService} from "../../services/contact-service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
-  contactForm = this.formBuilder.nonNullable.group({
-    name: [''],
-    mail: [''],
-    phone: [''],
-    message: ['', Validators.required],
-  })
+  submitted = false;
+  error = false;
+  contactInformation?: ContactInformation;
+  contactForm!: FormGroup;
 
   constructor(private contactService: ContactService,
               private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.contactForm = new FormGroup({
+      name: new FormControl("", Validators.required),
+      mail: new FormControl("", [Validators.required, Validators.email]),
+      phone: new FormControl(""),
+      message: new FormControl("", Validators.required)
+      }
+    )
   }
 
   onSubmit() {
@@ -30,29 +40,16 @@ export class ContactComponent {
       let contactInformation: ContactInformation = new ContactInformation(
         this.contactForm.value.name, this.contactForm.value.mail, this.contactForm.value.phone, this.contactForm.value.message
       )
+      this.submitted = true;
       this.contactService.postContactForm(contactInformation).subscribe(({
-        next: () => {
-          console.log("Success");
+        next: (contactInformation: ContactInformation) => {
+          this.contactForm.reset();
+          this.contactInformation = contactInformation;
         }, error: err => {
-          console.log(err);
+          this.error = true;
         }
       }))
     }
-    console.warn(this.contactForm.value)
   }
 
-  /*
-  postMessage(form: NgForm) {
-    if (form.valid) {
-      this.contactService.postContactForm(this.contactInformation).subscribe({
-        next: () => {
-
-        }
-      });
-    }
-  }
-
-   */
-
-  protected readonly onsubmit = onsubmit;
 }
