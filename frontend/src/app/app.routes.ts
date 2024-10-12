d6ccee90cb1eb0a263cf1476412e7721014354f4
Janis Schneeberger <janis.schneeberger@outlook.com>
2024-10-12 14:06:07 +0200

    FEAT: Added simple Interests page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
 import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 import {ResumeComponent} from "./components/resume/resume.component";
+import {InterestComponent} from "./components/interest/interest.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'contact', component: ContactComponent },
   { path: 'resume', component: ResumeComponent},
+  { path: 'interest', component: InterestComponent},
   { path: '**', component: PageNotFoundComponent}
 ];
