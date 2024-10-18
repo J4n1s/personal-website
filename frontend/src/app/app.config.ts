c53c1c38d9209b5aff4b4fbd2684158e41cd0667
Janis Schneeberger <janis.schneeberger@outlook.com>
2024-10-18 13:59:31 +0200

    FEAT: Disabled SSR and added HTTP Language Interceptor

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {LanguageInterceptor} from "./interceptors/language.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    { provide: HTMLAllCollection, useClass: LanguageInterceptor, multi: true },
   ]
 };
