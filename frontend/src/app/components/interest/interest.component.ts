b9688fad1e24f68f2f134165d32eedf905c17b52
Janis Schneeberger <janis.schneeberger@outlook.com>
2024-10-18 10:55:05 +0200

    REFACTOR: Removed last unnecessary divider on interests page

--- frontend/src/app/components/interest/interest.component.ts
+++ frontend/src/app/components/interest/interest.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ContentService} from "../../services/content.service";
 import {HttpErrorResponse} from "@angular/common/http";
-import {NgClass, NgForOf, NgOptimizedImage} from "@angular/common";
+import {NgClass, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {Globals} from "../../globals";
 
 @Component({
@@ -10,7 +10,8 @@ import {Globals} from "../../globals";
   imports: [
     NgForOf,
     NgClass,
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgIf
   ],
   templateUrl: './interest.component.html',
   styleUrl: './interest.component.scss'
