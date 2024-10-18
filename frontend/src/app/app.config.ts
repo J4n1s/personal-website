bebc09e385d17dcff6f4769962c8840984c45da7
Janis Schneeberger <janis.schneeberger@outlook.com>
2024-10-18 17:58:34 +0200

    REFACTOR: Changed Config after removal of SSR

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
+import {provideHttpClient, withFetch} from "@angular/common/http";
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import {LanguageInterceptor} from "./interceptors/language.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(withFetch()),
     { provide: HTMLAllCollection, useClass: LanguageInterceptor, multi: true },
   ]
