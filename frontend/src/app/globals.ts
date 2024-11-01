1379acb903554ed82f1e1e82c95c0654aad6d4f0
Janis Schneeberger <janis.schneeberger@outlook.com>
2024-11-01 12:58:20 +0100

    REFACTOR: Dynamic query of backend URL

--- frontend/src/app/globals.ts
+++ frontend/src/app/globals.ts
@@ -6,20 +6,17 @@ import {Injectable} from '@angular/core';
 export class Globals {
 
   readonly baseUri: string = this.findBackendUrl();
-  readonly apiUri: string = this.baseUri + 'api/v1';
+  readonly apiUri: string = this.baseUri + '/api/v1';
 
   readonly userLanguage: string = this.findLanguage();
 
   private findBackendUrl(): string {
-    return 'http://localhost:8080/';
-    /*
     if (window.location.port === '4200') { // local `ng serve`, backend at localhost:8080
-      return 'http://localhost:8080/api';
+      return 'http://localhost:8080';
     } else {
       // assume deployed somewhere and backend is available at same host/port as frontend
-      return window.location.protocol + '//' + window.location.host + window.location.pathname + 'api';
+      return window.location.protocol + '//' + window.location.host;
     }
-     */
   }
 
   private findLanguage(): string {
